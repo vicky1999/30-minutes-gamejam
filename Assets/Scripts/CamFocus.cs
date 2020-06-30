aebc231698038b06730ca46fbbbb31eb57109d56
vicky1999 <ravivignesh2000@gmail.com>
2020-06-30 21:43:31 +0530

    updated

---
+++ Assets/Scripts/CamFocus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFocus : MonoBehaviour
+{
+	public Transform Player;
+    
+	void LateUpdate()
+	{
+		transform.position = new Vector3(Player.transform.position.x, transform.position.y, -10f);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+	public float speed=0.5f;
+	public GameObject ground;
+	private int count=0;
+	public GameObject obstacles;
+    private bool isJump=false;
+    public Rigidbody2D body;
+    private float pos=24f;
+    // Update is called once per frame
+    void Awake() {
+    	body=this.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+    	this.transform.position=new Vector2(transform.position.x+speed*Time.deltaTime,this.transform.position.y); 
+    	if(this.transform.position.x > count * 51f) {
+    		count+=1;
+    		pos+=51f;
+    		Object.Instantiate(ground, new Vector3(pos,-4.47f,0f),Quaternion.identity);
+    	}
+ 		if (Input.GetKeyDown(KeyCode.Space) && !isJump) {
+    		body.AddForce(transform.up*7f,ForceMode2D.Impulse);
+    		isJump=true;
+    	}
+    }
+
+	void OnCollisionEnter2D(Collision2D col) {
+	    if(col.gameObject.tag == "ground")
+	    {
+	        isJump = false;
+	    }
+	    if(col.gameObject.tag=="obstacles") {
+	    	SceneManager.LoadScene("SampleScene");
+	    }
+	}
+}
+
